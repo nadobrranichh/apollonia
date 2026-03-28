7453f4235143fdf24970f4fdf67b229e2fe1f9ec
nadobrranichh <nazarshch26@gmail.com>
2026-03-28 14:19:49 +0200

    feat: added translation recources (english, ukrainian locales)

--- src/internationalization/i18n.ts
+++ src/internationalization/i18n.ts
@@ -1,13 +1,12 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-
-import english from "./locales/english.json";
 import ukrainian from "./locales/ukrainian.json";
+import english from "./locales/english.json";
 
 i18n.use(initReactI18next).init({
   resources: {
-    en: { translation: english },
     uk: { translation: ukrainian },
+    en: { translation: english },
   },
   lng: "en",
   fallbackLng: "en",
