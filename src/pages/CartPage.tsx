beedc78b3f7cf8d18eba71b2e9da2a016536b61d
nadobrranichh <nazarshch26@gmail.com>
2026-04-17 05:34:12 +0300

    fix: fixed subtotal calculation on cart page

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -4,12 +4,25 @@ import { useCartStore } from "../store/cart-store";
 import CartItemContainer from "../components/shop/CartItemContainer";
 import { pageTitleStyle } from "../styles/typographyStyles";
 import { useTranslation } from "react-i18next";
+import { useQuery } from "@tanstack/react-query";
+import { configureConversionRate } from "../http/http";
 export default function CartPage() {
   const { t } = useTranslation();
   const { cart, removeItem, updateQuantity } = useCartStore();
 
-  const subTotal = Math.round(
-    cart.reduce((acc, el) => acc + el.quantity * el.price_in_cents, 0) / 100,
+  const { data: conversionRate } = useQuery({
+    queryFn: configureConversionRate,
+    queryKey: ["conversionRate"],
+    staleTime: Infinity, // remains fresh
+    gcTime: Infinity, // doesn't get garbage collected
+    placeholderData: { rate: 1, currency: "CAD" },
+  });
+  const { rate, currency } = conversionRate!;
+
+  const subTotal = cart.reduce(
+    (acc, el) =>
+      acc + el.quantity * Math.round((el.price_in_cents / 100) * rate),
+    0,
   );
   return (
     <Box
@@ -56,7 +69,7 @@ export default function CartPage() {
               }}
             >
               <Typography sx={{ fontWeight: 500, fontSize: "1.5rem" }}>
-                {t("cart.subtotal")}: ${subTotal}
+                {t("cart.subtotal")}: ${subTotal} {currency}
               </Typography>
               <Button
                 component={RouterLink}
