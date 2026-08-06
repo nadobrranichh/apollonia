411fcbe162fe92155b0668235cbd2b8af12aff08
nadobrranichh <nazarshch26@gmail.com>
2026-08-06 19:40:59 +0300

    feat: added sections with available treatments, added all created content to the home page

---
+++ src/components/TreatmentCard.tsx
@@ -0,0 +1,114 @@
+import { Box, Card, Link, Typography } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import ImageBox from "./ImageBox";
+
+export default function TreatmentCard({
+  imageSrc,
+  title,
+  description,
+  price,
+  time,
+}: {
+  imageSrc?: string | string[];
+  title: string;
+  description: string;
+  price: string;
+  time?: string;
+}) {
+  return (
+    <Card
+      sx={{
+        borderRadius: "1.5rem",
+        padding: "1.5rem",
+        boxShadow: (theme) => `0px 0px 0.5rem ${theme.palette.grey[500]}`,
+      }}
+    >
+      <Box
+        sx={{
+          height: "15rem",
+          backgroundColor: "secondary.light",
+          margin: "-1.5rem -1.5rem 1.5rem",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {imageSrc &&
+          (Array.isArray(imageSrc) ? (
+            imageSrc.map((img) => (
+              <ImageBox
+                src={img}
+                height="3rem"
+                sx={{
+                  filter: (theme) =>
+                    `drop-shadow(0 0 2rem ${theme.palette.grey[600]})`,
+                }}
+              />
+            ))
+          ) : (
+            <ImageBox
+              src={imageSrc}
+              height="7.5rem"
+              sx={{
+                filter: (theme) =>
+                  `drop-shadow(0 0 2rem ${theme.palette.grey[600]})`,
+              }}
+            />
+          ))}
+      </Box>
+      <Box sx={{ display: "flex", flexDirection: "column", gap: "0.5rem" }}>
+        <Typography variant="h4">{title}</Typography>
+        <Typography variant="body2" sx={{ fontStyle: "italic" }}>
+          {description}
+        </Typography>
+        <hr />
+        <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+          {time ? (
+            <Box sx={{ display: "flex", gap: "0.5rem" }}>
+              <Typography sx={{ fontFamily: "Poppins, Arial" }}>
+                ${price}
+              </Typography>
+              <Typography
+                sx={{
+                  color: (theme) => theme.palette.grey[500],
+                  transform: "translateY(-3px)",
+                }}
+              >
+                -
+              </Typography>
+              <Typography
+                sx={{
+                  textTransform: "uppercase",
+                  color: (theme) => theme.palette.grey[500],
+                  fontFamily: "Poppins, Arial",
+                }}
+              >
+                {time}.
+              </Typography>
+            </Box>
+          ) : (
+            <Typography sx={{ fontFamily: "Poppins, Arial" }}>
+              ${price}
+            </Typography>
+          )}
+
+          <Link
+            variant="body2"
+            component={RouterLink}
+            // to={`/treatments#${title.toLowerCase().replaceAll(" ", "-")}`}
+            to="/"
+            sx={{
+              textDecoration: "none",
+              padding: "0.2rem",
+              textTransform: "uppercase",
+              transform: "translateY(-3px)",
+            }}
+          >
+            Explore
+          </Link>
+        </Box>
+      </Box>
+    </Card>
+  );
+}
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,98 +1,116 @@
 import { Box, Typography } from "@mui/material";
-import VerticalHeroImg1 from "../assets/IMG_2780-s.jpg";
-import VerticalHeroImg2 from "../assets/IMG_2779-s.jpg";
-import HorizontalHeroImg from "../assets/IMG_2777-s.jpg";
-import ImageBox from "../components/ImageBox";
-import { useTranslation } from "react-i18next";
+import HeroSection from "../components/home/HeroSection";
+import WhiteningImg from "../assets/regular-whitening-teeth.png";
+import TreatmentCard from "../components/TreatmentCard";
+import SwarovskiCrystalsImg from "../assets/tooth-gems.png";
+import GoldenToothGemsImg from "../assets/golden-tooth-gems.png";
+import WhiteningTrayImg from "../assets/whitening-tray-cropped.png";
+import SportsguardImg from "../assets/sportsguard.png";
+import MethodSection from "../components/home/MethodSection";
+import FounderQuoteSection from "../components/home/FounderQuoteSection";
+import ReviewsSection from "../components/home/ReviewsSection";
+import MetricsSection from "../components/home/MetricsSection";
 
 export default function HomePage() {
-  const { t } = useTranslation();
   return (
     <Box
       component="main"
-      sx={{ padding: "6% 0", display: "flex", justifyContent: "center" }}
+      sx={{
+        overflow: "hidden",
+      }}
     >
-      <Box
-        sx={{
-          width: { xs: "85%", sm: "80%", md: "65%", lg: "45rem" },
-          "* + *": {
-            marginTop: { xs: "1rem", sm: "none" },
-          },
-        }}
-      >
-        <Box
+      <HeroSection />
+      <MetricsSection />
+      <FounderQuoteSection />
+      <MethodSection />
+
+      {/* signature treatments section */}
+      <Box sx={{ padding: "0 1.5rem 4rem" }}>
+        <Typography variant="h3">
+          Our{" "}
+          <Box component="span" sx={{ color: "secondary.main" }}>
+            signature
+          </Box>{" "}
+          treatments:
+        </Typography>
+        <Typography
           sx={{
-            display: "grid",
-            gridTemplateColumns: "1fr 2fr",
-            boxSizing: "border-box",
-            alignItems: "start",
-            gap: 2.5,
+            fontStyle: "italic",
+            marginBottom: "1rem",
           }}
         >
-          <ImageBox
-            src={VerticalHeroImg1}
-            sx={{
-              height: { xs: "15rem", sm: "20rem" },
-              objectFit: "cover",
-              borderRadius: "1.25rem",
-            }}
+          Designed for brighter smiles with comfort, precision, and lasting
+          results.
+        </Typography>
+
+        <Box sx={{ display: "flex", flexDirection: "column", gap: "2rem" }}>
+          <TreatmentCard
+            imageSrc={WhiteningImg}
+            title="Painless Teeth Whitening Treatment"
+            description="LED whitening with red light therapy (anti inflammatory), gum
+              protection and anti-sensitivity dental grade materials (fluoride
+              and hydroxyapatite)."
+            price={"165"}
+            time="70 min"
           />
-          <Typography
-            sx={{
-              fontSize: { xs: "1rem", sm: "1.3rem" },
-              textAlign: "end",
-              marginTop: "0.5rem",
-              color: "primary.contrastText",
-            }}
-          >
-            {t("home.paragraph1")}
-          </Typography>
-        </Box>
-        <Box
-          sx={{
-            display: "grid",
-            gridTemplateColumns: "2fr 1fr",
-            boxSizing: "border-box",
-            gap: 2.5,
-            alignItems: "start",
-          }}
-        >
-          <Typography
-            sx={{
-              fontSize: { xs: "1rem", sm: "1.3rem" },
-              textAlign: "start",
-              marginTop: { xs: 0, md: "1.5rem" },
-              color: "primary.contrastText",
-            }}
-          >
-            {t("home.paragraph2")}
-          </Typography>
-          <ImageBox
-            src={VerticalHeroImg2}
-            sx={{
-              transform: {
-                xs: 0,
-                md: "translateY(-25%)",
-                lg: "translateY(-40%)",
-              },
-              height: { xs: "15rem", sm: "20rem" },
-              objectFit: "cover",
-              justifySelf: { xs: "auto", md: "end" },
-              margin: 0,
-              marginLeft: { xs: "auto", md: "none" },
-              borderRadius: "1.25rem",
-            }}
+          <TreatmentCard
+            imageSrc={WhiteningImg}
+            title="Professional Teeth Cleaning"
+            description="Ultrasonic scaling followed by manual scaling and polishing.Add-ons as per client request: air polishing, AirFlow cleaning and fluoride."
+            price={"165"}
+            time="70 min"
+          />
+          <TreatmentCard
+            imageSrc={WhiteningImg}
+            title="White Spot Lesions Removal"
+            description="Icon® Infiltration Concept is used for the microinvasive treatment of white dental lesions. In one patient visit, and with no drilling, Icon® can arrest the progression of early enamel white spot caries-like lesions (demineralization)."
+            price={"80"}
+            time="70 min"
           />
         </Box>
-        <ImageBox
-          src={HorizontalHeroImg}
-          width="100%"
-          sx={{
-            marginTop: { xs: "none", md: "-3rem", lg: "-6rem" },
-            borderRadius: "2.5rem",
-          }}
-        />
       </Box>
+
+      {/* dental accesories section */}
+      <Box sx={{ padding: "0 1.5rem 4rem" }}>
+        <Typography variant="h3">
+          Our dental{" "}
+          <Box component="span" sx={{ color: "secondary.main" }}>
+            accessories:
+          </Box>
+        </Typography>
+        <Typography
+          sx={{
+            fontStyle: "italic",
+            marginBottom: "1rem",
+          }}
+        >
+          Crafted to complement your treatment and support your smile long after
+          your appointment.
+        </Typography>
+
+        <Box sx={{ display: "flex", flexDirection: "column", gap: "2rem" }}>
+          <TreatmentCard
+            imageSrc={[SwarovskiCrystalsImg, GoldenToothGemsImg]}
+            title="Tooth gems"
+            description="Swarovski crystals and 18K tooth gems available for temporary (2-7 days) and semi-permanent (3-12 months) application. The procedure is done with no drilling. Gems are applied with dental bond and orthodontic composite resin. Can be polished off any time as needed. +$20 for each additional gem."
+            price={"40-90"}
+          />
+          <TreatmentCard
+            imageSrc={WhiteningTrayImg}
+            title="Custom Whitening Trays"
+            description="Designed to hold a whitening gel against your teeth, helping to lighten stains and discoloration."
+            price={"200"}
+          />
+          <TreatmentCard
+            imageSrc={SportsguardImg}
+            title="Sportsguard"
+            description="Custom fit protective device worn over the teeth — typically covering the upper teeth — to safeguard the mouth from injuries during sports and physical activities."
+            price={"220"}
+          />
+        </Box>
+      </Box>
+
+      <ReviewsSection />
     </Box>
   );
 }
