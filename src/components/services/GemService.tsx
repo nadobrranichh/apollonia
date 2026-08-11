d04e43cbd7f647779a7e75d3b01d12989a4f679b
nadobrranichh <nazarshch26@gmail.com>
2026-08-11 19:52:39 +0300

    feat: redesigned gems service component

--- src/components/services/GemService.tsx
+++ src/components/services/GemService.tsx
@@ -1,92 +1,106 @@
-import { Box, Button, Typography, useMediaQuery } from "@mui/material";
 import {
-  gemServiceSubtitleStyles,
-  serviceStyles,
-  serviceTitleStyles,
-  gemServiceDescriptionContainerStyles,
-} from "../../styles/servicesStyles";
+  Box,
+  Button,
+  Card,
+  Divider,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
 import ToothGemsImg from "../../assets/tooth-gems.png";
 import GoldenToothGemsImg from "../../assets/golden-tooth-gems.png";
-import ServicePriceContainer from "./ServicePriceContainer";
 import ImageBox from "../ImageBox";
 import { useTranslation } from "react-i18next";
+import {
+  captionStyles,
+  servicePriceStyles,
+} from "../../styles/typographyStyles";
 
 export default function GemService() {
   const { t } = useTranslation();
   const isMobile = useMediaQuery("(max-width: 1000px)");
   return (
-    <Box sx={{ ...serviceStyles, paddingTop: "0.75rem" }}>
-      <Box sx={gemServiceDescriptionContainerStyles}>
-        <Box sx={{ marginLeft: { xs: "0.7rem", md: "1.2rem" } }}>
-          <Typography sx={serviceTitleStyles}>
-            {t("services.toothGems.title")} {"\u2005"}
-            <span
-              style={{
-                textTransform: "none",
-                fontSize: isMobile ? "0.7rem" : "1rem",
-              }}
-            >
-              {t("services.toothGems.titleComment")}
-            </span>
-          </Typography>
-          <Typography sx={serviceTitleStyles}>
-            {t("services.toothGems.swarovskiCrystals.title")}
-          </Typography>
-          <Typography sx={gemServiceSubtitleStyles}>
-            <b>{t("services.toothGems.swarovskiCrystals.description1")}</b>{" "}
-            {t("services.toothGems.swarovskiCrystals.description2")} <br />
-            {t("services.toothGems.swarovskiCrystals.description3")}
-            <ImageBox
-              src={ToothGemsImg}
-              sx={{
-                position: "absolute",
-                width: { xs: "7.5rem", md: "12rem" },
-                marginLeft: { xs: "5px", md: "15px" },
-              }}
-            />
-          </Typography>
-        </Box>
-        <ServicePriceContainer
-          price={import.meta.env.VITE_SERVICE_SWAROVSKI_CRYSTALS_PRICE}
-        />
-      </Box>
+    <Card
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "0.5rem",
+      }}
+    >
       <Box
-        sx={{ ...gemServiceDescriptionContainerStyles, marginTop: "0.5rem" }}
-      >
-        <Box sx={{ marginLeft: { xs: "0.7rem", md: "1.2rem" } }}>
-          <ImageBox
-            src={GoldenToothGemsImg}
-            sx={{
-              position: "absolute",
-              width: { xs: "7.5rem", md: "12rem" },
-              marginLeft: { xs: "5rem", md: "7rem" },
-              marginTop: { xs: "1.2rem", md: "1.5rem" },
-            }}
-          />
-          <Typography sx={serviceTitleStyles}>
-            {t("services.toothGems.goldenToothGem.title")}
-          </Typography>
-          <Typography sx={gemServiceSubtitleStyles}>
-            {t("services.toothGems.goldenToothGem.description")}
-          </Typography>
-        </Box>
-        <ServicePriceContainer
-          price={import.meta.env.VITE_SERVICE_GOLDEN_18K_GEM_PRICE}
-        />
-      </Box>
-      <Typography
         sx={{
-          textAlign: "center",
-          fontSize: { xs: "0.8rem", md: "1rem" },
-          mx: "0.75rem",
-          mt: "0.75rem",
+          height: "8rem",
+          background: (theme) =>
+            `linear-gradient(90deg, ${theme.palette.secondary.main}, ${theme.palette.secondary.dark})`,
+          margin: "-1.2rem -1.2rem 0",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "0.5rem",
         }}
       >
+        <ImageBox src={ToothGemsImg} sx={{ height: "2rem" }} />
+        <ImageBox src={GoldenToothGemsImg} sx={{ height: "2rem" }} />
+      </Box>
+      <Box>
+        <Typography sx={{ fontWeight: "bold" }}>
+          {t("services.toothGems.title")}
+        </Typography>
+        <Typography sx={captionStyles}>
+          {t("services.toothGems.titleComment")}
+        </Typography>
+      </Box>
+      <Divider />
+      <Box>
+        <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+          <Typography sx={{ fontWeight: "bold" }}>
+            {t("services.toothGems.swarovskiCrystals.title")}{" "}
+          </Typography>
+          <Typography sx={servicePriceStyles}>
+            ${import.meta.env.VITE_SERVICE_SWAROVSKI_CRYSTALS_PRICE}
+          </Typography>
+        </Box>
+        <Typography sx={captionStyles}>
+          &bull; {t("services.toothGems.swarovskiCrystals.description2")}
+        </Typography>
+        <Typography sx={captionStyles}>
+          &bull; {t("services.toothGems.swarovskiCrystals.description3")}
+        </Typography>
+        {/* <ImageBox
+          src={ToothGemsImg}
+          sx={{ width: { xs: "7.5rem", md: "12rem" } }}
+        /> */}
+      </Box>
+      <Divider />
+      {/* 
+      
+      */}
+      <Box>
+        <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+          <Typography sx={{ fontWeight: "bold" }}>
+            {t("services.toothGems.goldenToothGem.title")}{" "}
+          </Typography>
+          <Typography sx={servicePriceStyles}>
+            ${import.meta.env.VITE_SERVICE_GOLDEN_18K_GEM_PRICE}
+          </Typography>
+        </Box>
+        <Typography sx={captionStyles}>
+          {t("services.toothGems.goldenToothGem.description")}
+        </Typography>
+        {/* <ImageBox
+          src={GoldenToothGemsImg}
+          sx={{
+            width: { xs: "7.5rem", md: "12rem" },
+          }}
+        /> */}
+      </Box>
+
+      <Typography sx={{ fontSize: "0.9rem", marginBottom: "1rem" }}>
         {t("services.toothGems.description")}
       </Typography>
       <Button
         variant="contained"
-        sx={{ my: "1rem" }}
+        sx={{ width: "100%" }}
         href={
           isMobile
             ? `sms:+16475141552?body=${encodeURIComponent(
@@ -95,8 +109,8 @@ export default function GemService() {
             : "https://www.instagram.com/apollonia_whitening"
         }
       >
-        BOOK NOW
+        Book Now
       </Button>
-    </Box>
+    </Card>
   );
 }
