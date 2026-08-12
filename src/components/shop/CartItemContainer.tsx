3419b16ff1667973128403dce5aedc970e5de7ce
nadobrranichh <nazarshch26@gmail.com>
2026-08-12 07:20:14 +0300

    feat: redesigned cart page

--- src/components/shop/CartItemContainer.tsx
+++ src/components/shop/CartItemContainer.tsx
@@ -1,26 +1,16 @@
-import { Box, Typography, IconButton, useMediaQuery } from "@mui/material";
+import { Box, Typography, IconButton, Card, Stack } from "@mui/material";
 import ImageBox from "../ImageBox";
-import type { CartItem } from "../../store/cart-store";
+import { useCartStore, type CartItem } from "../../store/cart-store";
 import { useState } from "react";
-import CrossIcon from "../../assets/cross-light-svgrepo-com.svg";
-import QuantityControls from "./QuantityControls";
-import { useTranslation } from "react-i18next";
 import { useQuery } from "@tanstack/react-query";
 import { configureConversionRate } from "../../http/http";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import CloseIcon from "@mui/icons-material/Close";
 
-export default function CartItemContainer({
-  item,
-  updateQuantity,
-  removeItem,
-}: {
-  item: CartItem;
-  updateQuantity: (id: number, newQuantity: number) => void;
-  removeItem: (id: number) => void;
-}) {
-  const { t } = useTranslation();
+export default function CartItemContainer({ item }: { item: CartItem }) {
+  const { updateQuantity, removeItem } = useCartStore();
   const [ShowImageSkeleton, setShowImageSkeleton] = useState<boolean>(true);
-
-  const isMd = useMediaQuery("(min-width: 1000px)");
 
   const { data: conversionRate } = useQuery({
     queryFn: configureConversionRate,
@@ -32,126 +22,68 @@ export default function CartItemContainer({
   const { rate, currency } = conversionRate!;
 
   return (
-    <Box sx={{ border: "1px solid white", display: { md: "flex" } }}>
-      <Box sx={{ display: "flex", gap: { xs: "0.5rem", md: "1rem" } }}>
-        <ImageBox
-          src={
-            item.image_urls ? item.image_urls[0] : "https://picsum.photos/800"
-          }
-          alt="icon"
-          style={{
-            width: "10rem",
-            height: "10rem",
-            objectFit: "cover",
-            display: ShowImageSkeleton ? "none" : "block",
-          }}
-          onLoad={() => setShowImageSkeleton(false)}
-        />
-        <Box
-          sx={{
-            paddingTop: { xs: "0.5rem", md: "1rem" },
-            flex: 1,
-          }}
-        >
-          <Typography
-            sx={{
-              fontWeight: 600,
-              fontSize: "1.2rem",
-              textTransform: "uppercase",
-            }}
-          >
-            {item.title}
-          </Typography>
-          <Typography
-            sx={{
-              fontWeight: 400,
-              fontSize: "1rem",
-              mt: "0.5rem",
-            }}
-          >
-            {t("cart.pricePerItem")}: $
-            {Math.round((item.price_in_cents / 100) * rate)} {currency}
-          </Typography>
-        </Box>
-      </Box>
+    <Card sx={{ position: "relative", padding: "0.75rem" }}>
+      <IconButton
+        sx={{ position: "absolute", top: "0.75rem", right: "0.75rem" }}
+        onClick={() => removeItem(item.id)}
+      >
+        <CloseIcon />
+      </IconButton>
 
-      {isMd ? (
-        <>
-          {item.max_quantity > 1 && (
-            <QuantityControls
-              quantity={item.quantity}
-              updateQuantity={(newQuantity: number) =>
-                updateQuantity(item.id, newQuantity)
-              }
-            />
-          )}
-          <Box
-            sx={{
-              display: "flex",
-
-              justifyContent: "end",
-              alignItems: "center",
-              mr: "2rem",
-              ml: item.max_quantity > 1 ? 0 : "auto",
-              width: "6.5rem",
-            }}
-          >
-            <Typography
-              sx={{
-                fontWeight: 400,
-                fontSize: "1.2rem",
-              }}
-            >
-              ${item.quantity * Math.round((item.price_in_cents / 100) * rate)}
-            </Typography>
-            <IconButton sx={{ padding: 0 }} onClick={() => removeItem(item.id)}>
-              <ImageBox
-                src={CrossIcon}
-                alt="icon"
-                sx={{ width: "1.25rem", marginLeft: "0.75rem" }}
-              />
-            </IconButton>
-          </Box>
-        </>
-      ) : (
-        <Box
-          sx={{
-            borderTop: "1px solid white",
-            display: "flex",
-            justifyContent: "space-around",
-            paddingY: "0.5rem",
-          }}
-        >
-          <QuantityControls
-            quantity={item.quantity}
-            updateQuantity={(newQuantity: number) =>
-              updateQuantity(item.id, newQuantity)
+      <Stack direction="row" spacing={2}>
+        <Box>
+          <ImageBox
+            src={
+              item.image_urls ? item.image_urls[0] : "https://picsum.photos/800"
             }
-          />
-          <Box
             sx={{
-              display: "flex",
-              alignItems: "center",
+              borderRadius: "1rem",
+              width: "6rem",
+              aspectRatio: "1/1",
+              objectFit: "cover",
+              display: ShowImageSkeleton ? "none" : "block",
+            }}
+            onLoad={() => setShowImageSkeleton(false)}
+            alt="icon"
+          />
+        </Box>
+
+        <Stack spacing={1} sx={{ width: "100%" }}>
+          <Typography sx={{ fontWeight: "bold" }}>{item.title}</Typography>
+
+          <Typography variant="body2" sx={{ fontFamily: "Poppins, Arial" }}>
+            ${Math.round((item.price_in_cents / 100) * rate) * item.quantity}{" "}
+            {currency}{" "}
+            {item.quantity > 1 &&
+              `($${Math.round((item.price_in_cents / 100) * rate)} ${currency} per
+            item)`}
+          </Typography>
+          <Stack
+            direction="row"
+            spacing={1}
+            sx={{
+              padding: "0.25rem",
+              border: (theme) => `1px solid ${theme.palette.primary.main}`,
+              borderRadius: "1rem",
+              alignSelf: "end",
             }}
           >
-            <Typography
-              sx={{
-                fontWeight: 400,
-                fontSize: "1.2rem",
-              }}
+            <IconButton
+              onClick={() => updateQuantity(item.id, item.quantity - 1)}
             >
-              ${item.quantity * Math.round((item.price_in_cents / 100) * rate)}
-            </Typography>
-            <IconButton sx={{ padding: 0 }} onClick={() => removeItem(item.id)}>
-              <ImageBox
-                src={CrossIcon}
-                alt="icon"
-                sx={{ width: "1.25rem", marginLeft: "0.75rem" }}
-              />
+              <RemoveIcon />
             </IconButton>
-          </Box>
-        </Box>
-      )}
-    </Box>
+            <Typography variant="body2" sx={{ fontFamily: "Poppins, Arial" }}>
+              {item.quantity}
+            </Typography>
+            <IconButton
+              onClick={() => updateQuantity(item.id, item.quantity + 1)}
+            >
+              <AddIcon />
+            </IconButton>
+          </Stack>
+        </Stack>
+      </Stack>
+    </Card>
   );
 }
--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -2,13 +2,13 @@ import { Box, Typography, Button } from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
 import { useCartStore } from "../store/cart-store";
 import CartItemContainer from "../components/shop/CartItemContainer";
-import { pageTitleStyle } from "../styles/typographyStyles";
 import { useTranslation } from "react-i18next";
 import { useQuery } from "@tanstack/react-query";
 import { configureConversionRate } from "../http/http";
+import { servicePriceStyles } from "../styles/typographyStyles";
 export default function CartPage() {
   const { t } = useTranslation();
-  const { cart, removeItem, updateQuantity } = useCartStore();
+  const { cart } = useCartStore();
 
   const { data: conversionRate } = useQuery({
     queryFn: configureConversionRate,
@@ -25,80 +25,42 @@ export default function CartPage() {
     0,
   );
   return (
-    <Box
-      component="main"
-      sx={{
-        paddingY: "2rem",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-      <Typography sx={pageTitleStyle}>{t("cart.title")}</Typography>
-      <Box
-        sx={{
-          marginY: "4rem",
-          width: { xs: "90%", md: "85%", lg: "70%", xl: "55%" },
-        }}
-      >
+    <Box component="main" sx={{ padding: "3rem 1.8rem" }}>
+      <Typography variant="h3">{t("cart.title")}</Typography>
+      <Box>
         {cart.length > 0 ? (
           <>
             <Box
               sx={{
-                "& > :not(:last-child)": {
-                  marginBottom: "2rem",
-                },
+                display: "flex",
+                flexDirection: "column",
+                gap: "2rem",
               }}
             >
               {cart.map((cartItem) => (
-                <CartItemContainer
-                  key={cartItem.id}
-                  item={cartItem}
-                  updateQuantity={updateQuantity}
-                  removeItem={removeItem}
-                />
+                <CartItemContainer key={cartItem.id} item={cartItem} />
               ))}
-            </Box>
-            <Box
-              sx={{
-                marginTop: "4rem",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                gap: "1rem",
-              }}
-            >
-              <Typography sx={{ fontWeight: 500, fontSize: "1.5rem" }}>
-                {t("cart.subtotal")}: ${subTotal} {currency}
-              </Typography>
-              <Button
-                component={RouterLink}
-                to="/checkout"
-                sx={{ padding: "0.7rem 2rem", textDecoration: "none" }}
-                variant="contained"
-              >
-                {t("cart.checkout")}
-              </Button>
+              <Box>
+                <Typography sx={{ ...servicePriceStyles, textAlign: "center" }}>
+                  {t("cart.subtotal")}: ${subTotal} {currency}
+                </Typography>
+                <Button
+                  component={RouterLink}
+                  to="/checkout"
+                  sx={{ width: "100%" }}
+                  variant="contained"
+                >
+                  {t("cart.checkout")}
+                </Button>
+              </Box>
             </Box>
           </>
         ) : (
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              gap: "2rem",
-            }}
-          >
-            <Typography sx={{ fontSize: "1.2rem" }}>
+          <Box sx={{ textAlign: "center", paddingY: "4rem" }}>
+            <Typography sx={{ marginBottom: "2rem" }}>
               {t("cart.noItems")}
             </Typography>
-            <Button
-              variant="contained"
-              component={RouterLink}
-              sx={{ padding: "0.7rem 1.5rem" }}
-              to="/shop"
-            >
+            <Button variant="contained" component={RouterLink} to="/shop">
               {t("cart.continueShopping")}
             </Button>
           </Box>
